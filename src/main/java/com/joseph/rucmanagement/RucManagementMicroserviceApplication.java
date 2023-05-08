da70ae55974c9eb70c3719088e08203667d62a6a
JosephGL <joseph.guzman.6@hotmail.com>
2023-05-07 19:18:10 -0500

    Spring boot archetype

---
+++ src/main/java/com/joseph/rucmanagement/RucManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.joseph.rucmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RucManagementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RucManagementMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseph/rucmanagement/RucManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseph.rucmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RucManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
